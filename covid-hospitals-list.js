2803152b6efaeba7727e82b92637b1a98f59eb6c
Gourav-Kumar-1107 <150685077+Gourav-Kumar-1107@users.noreply.github.com>
2024-03-12 00:39:47 +0530

    Add files via upload

---
+++ covid-hospitals-list.js
@@ -0,0 +1,455 @@
+let hospitals = [{
+    "name": "Jayanagar General Hospital",
+    "phoneNumber":"080 2244 5711",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9262377,
+        "longitude": 77.5906121, 
+    },
+
+    "address":"32nd Cross, 4th B Block, beside Rajiv Gandhi Health Institute, Tilak Nagar, Jayanagar, Bengaluru, Karnataka 560041",
+    "addressLines":[
+        "32nd Cross, 4th B Block", 
+        "beside Rajiv Gandhi Health Institute", 
+        "Tilak Nagar, Jayanagar", 
+        "Bengaluru, Karnataka 560041"],
+    "pincode": "560041",
+    "zipCode":"56004", /* first few digits */
+    "numBeds" : "5",
+    "rating" : "3.6",
+},
+
+{
+    "name": "Epidemic Disease Hospital",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9860325,
+        "longitude": 77.6420924,
+    },
+    "address":"Indira Nagar I Stage, Hoysala Nagar, Indiranagar, Bengaluru, Karnataka 560038",
+    "addressLines":["Indira Nagar I Stage", "Hoysala Nagar", "Bengaluru, Karnataka 560038"],
+    "pincode": "560038",
+    "zipCode":"56003", /* first few digits */
+    "numBeds" : "10",
+    "rating" : "3.4",
+},
+
+{
+    "name": "Bowring and Lady Curzon Hospital",
+    "phoneNumber":"080 2559 1325",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.982188,
+        "longitude": 77.6018685,
+    },
+    "address":"XJJ3+VJ Shivaji Nagar, Bengaluru, Karnataka 560001",
+    "addressLines":["XJJ3+VJ Shivaji Nagar", "Bengaluru, Karnataka"],
+    "pincode": "560001",
+    "zipCode":"56000", /* first few digits */
+    "numBeds" : "2",
+    "rating" : "3.7",
+},
+
+
+{
+    "name": "KC General Hospital",
+    "phoneNumber":"080 2334 1771",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9961428,
+        "longitude": 77.5671693,
+    },
+    "address":"Ground, Malleswaram Circle, Police Station Rd, Bengaluru, Karnataka 560003",
+    "addressLines":["Ground, Malleswaram Circle", "Police Station Rd", "Bengaluru, Karnataka 560003"],
+    "pincode": "560003",
+    "zipCode":"56000", /* first few digits */
+    "numBeds" : "1",
+    "rating" : "3.1",
+},
+
+{
+    "name": "SDS Tuberculosis Research Center & Rajiv Gandhi Institute of Chest Diseases",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.938365,
+        "longitude": 77.5885142,
+    },
+    "address":"Someshwaranagara Dharmaram College Post, 1st Main Rd, near NIMHANS, Bengaluru, Karnataka 560029",
+    "addressLines":["Someshwaranagara Dharmaram College Post", "1st Main Rd, near NIMHANS", "Bengaluru, Karnataka 560029"],
+    "pincode": "560029",
+    "zipCode":"56002", /* first few digits */
+    "numBeds" : "12",
+    "rating" : "3",
+},
+
+
+{
+    "name": "Manipal Hospitals",
+    "phoneNumber":"080 2502 4444",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9583911,
+        "longitude": 77.6466477,
+    },
+    "address":"98, HAL Old Airport Rd, Kodihalli, Bengaluru, Karnataka 560017",
+    "addressLines":["98, HAL Old Airport Rd", "Kodihalli", "Bengaluru, Karnataka 560017"],
+    "pincode": "560017",
+    "zipCode":"56001", /* first few digits */
+    "numBeds" : "15",
+    "rating" : "4.1",
+},
+
+
+{
+    "name": "Narayana Health City",
+    "phoneNumber":"080675 06870",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.8277197,
+        "longitude": 77.6571877,
+    },
+    "address":"258/A, Bommasandra Industrial Area Anekal Taluk, Hosur Rd, Bengaluru, Karnataka 560099",
+    "addressLines":["258/A, Bommasandra Industrial Area Anekal Taluk", "Hosur Rd", "Bengaluru, Karnataka 560099"],
+    "pincode": "560099",
+    "zipCode":"56009", /* first few digits */
+    "numBeds" : "20",
+    "rating" : "4.4",
+},
+
+
+{
+    "name": "Fortis Hospital",
+    "phoneNumber":"096633 67253",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.8948227,
+        "longitude": 77.5964309,
+    },
+    "address":"154, 9, Bannerghatta Main Rd, Opposite IIM, Sahyadri Layout, Panduranga Nagar, Bengaluru, Karnataka 560076",
+    "addressLines":["154, 9, Bannerghatta Main Rd", "Opposite IIM, Sahyadri Layout", "Panduranga Nagar", "Bengaluru, Karnataka 560076"],
+    "pincode": "560076",
+    "zipCode":"56007", /* first few digits */
+    "numBeds" : "6",
+    "rating" : "4.4",
+},
+
+
+{
+    "name": "Victoria Hospital",
+    "phoneNumber":"080 2670 1150",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9635669,
+        "longitude": 77.5716187,
+    },
+    "address":"Fort Rd, near City Market, New Tharagupet, Bengaluru, Karnataka 560002",
+    "addressLines":["Fort Rd, near City Market", "New Tharagupet", "Bengaluru, Karnataka 560002"],
+    "pincode": "560002",
+    "zipCode":"56000", /* first few digits */
+    "numBeds" : "5",
+    "rating" : "3.5",
+},
+
+
+{
+    "name": "Sir.C.V. Raman General Hospital",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9829148,
+        "longitude": 77.6428149,
+    },
+    "address":"68/1, 4th B Cross Rd, Kalyan Nagar, Michael Palaya, H Colony, Indiranagar, Bengaluru, Karnataka 560038",
+    "addressLines":["68/1, 4th B Cross Rd", "Kalyan Nagar, Michael Palaya", "H Colony, Indiranagar", "Bengaluru, Karnataka 560038"],
+    "pincode": "560038",
+    "zipCode":"56003", /* first few digits */
+    "numBeds" : "7",
+    "rating" : "3.8",
+},
+
+{
+    "name": "ESIC Model Hospital",
+    "phoneNumber":"+918023325130",
+    "openTime" : "9am-4pm",
+    "coordinates": {
+        "latitude": 12.9916924,
+        "longitude": 77.551712,
+    },
+    "address":"620/A/1, 12th Main Rd, 2nd Block, Rajajinagar, Bengaluru, Karnataka 560010",
+    "addressLines":["620/A/1, 12th Main Rd", "2nd Block, Rajajinagar", "Bengaluru, Karnataka 560010"],
+    "pincode": "560010",
+    "zipCode":"56001", /* first few digits */
+    "numBeds" : "5",
+    "rating" : "2.9",
+},
+
+{
+    "name": "General Hospital Devanahalli",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 13.2568568,
+        "longitude": 77.7252711,
+    },
+    "address":"SH 96, Devanahalli, Binnamangala, Karnataka 562110",
+    "addressLines":["SH 96, Devanahalli", "Binnamangala, Karnataka 562110"],
+    "pincode": "562110",
+    "zipCode":"56211", /* first few digits */
+    "numBeds" : "4",
+    "rating" : "2.8",
+},
+
+{
+    "name": "BGS Global Institute of Medical Sciences",
+    "phoneNumber":"+918026984802",
+    "openTime" : "Temporarily closed",
+    "coordinates": {
+        "latitude": 12.9022857,
+        "longitude": 77.4956326,
+    },
+    "address":"No. 67, BGS Health & Education City, Uttarahalli Road, Kengeri, Bengaluru, Karnataka 560060",
+    "addressLines":["No. 67, BGS Health & Education City", "Uttarahalli Road, Kengeri", "Bengaluru, Karnataka 560060"],
+    "pincode": "560060",
+    "zipCode":"56006", /* first few digits */
+    "numBeds" : "7",
+    "rating" : "4.1",
+},
+
+{
+    "name": "Rajarajeshwari Medical College and Hospital",
+    "phoneNumber":"+918028437444",
+    "openTime" : "Temporatily closed",
+    "coordinates": {
+        "latitude": 12.8961554,
+        "longitude": 77.4596629,
+    },
+    "address":"202, Mysore Road Kambipura, Bengaluru, Karnataka 560074",
+    "addressLines":["202, Mysore Road Kambipura", "Bengaluru, Karnataka 560074"],
+    "pincode": "560074",
+    "zipCode":"56007", /* first few digits */
+    "numBeds" : "7",
+    "rating" : "3.6",
+},
+
+{
+    "name": "General Hospital,Nelamangala",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 13.0987354,
+        "longitude": 77.389022,
+    },
+    "address":"Jayanagara, Indiranagara, Karnataka 562123",
+    "addressLines":["Jayanagara, Indiranagara", "Karnataka 562123"],
+    "pincode": "562123",
+    "zipCode":"56212", /* first few digits */
+    "numBeds" : "5",
+    "rating" : "2.8",
+},
+
+{
+    "name": "General Hospital, Doddaballapur",
+    "phoneNumber":"Not Available",
+    "openTime" : "Not Available",
+    "coordinates": {
+        "latitude": 13.2843621,
+        "longitude": 77.5292349,
+    },
+    "address":"7GVV+2J Doddaballapura, Karnataka 561203",
+    "addressLines":["7GVV+2J Doddaballapura", "Karnataka 561203"],
+    "pincode": "561203",
+    "zipCode":"56120", /* first few digits */
+    "numBeds" : "4",
+    "rating" : "4.2",
+},
+
+{
+    "name": "General Hospital, Anekal",
+    "phoneNumber":"Not Available",
+    "openTime" : "9am-4:30pm",
+    "coordinates": {
+        "latitude": 12.7575095,
+        "longitude": 77.6367502,
+    },
+    "address":"Weaver Colony, Anekal, Karnataka 562106",
+    "addressLines":["Weaver Colony, Anekal", "Karnataka 562106"],
+    "pincode": "562106",
+    "zipCode":"56210", /* first few digits */
+    "numBeds" : "6",
+    "rating" : "4.1",
+},
+
+{
+    "name": "Ruby General Hospital, Kolkata",
+    "phoneNumber":"033-66011800",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.519883904253344,
+        "longitude": 88.40613338614214,
+    },
+    "address":"576, Anandapur Main Rd, Golpark, Sector I, Kasba, Kolkata, West Bengal 700107, India",
+    "addressLines":["576 Anandapur Main Rd", "Golpark, Sector I, Kasba, Kolkata", "West Bengal 700107, India"],
+    "pincode": "700107",
+    "zipCode":"700107", /* first few digits */
+    "numBeds" : "18",
+    "rating" : "4.6",
+},
+
+{
+    "name": "Ruby General Hospital, Kolkata",
+    "phoneNumber":"033-66011800",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.519883904253344,
+        "longitude": 88.40613338614214,
+    },
+    "address":"576, Anandapur Main Rd, Golpark, Sector I, Kasba, Kolkata, West Bengal 700107, India",
+    "addressLines":["576 Anandapur Main Rd", "Golpark, Sector I, Kasba, Kolkata", "West Bengal 700107, India"],
+    "pincode": "700107",
+    "zipCode":"700107", /* first few digits */
+    "numBeds" : "18",
+    "rating" : "4.6",
+},
+{
+    "name": "ESI Hospital - Budge Budge",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 22.494880933641088,
+        "longitude": 88.21921738308922,
+    },
+    "address":"B1-97, New, Budge Budge Trunk Rd, opp. ESI Hospital, Gumar Gala, Kolkata, Maheshtala, West Bengal 700140, India",
+    "addressLines":["B1-97, New, Budge Budge Trunk Rd", "opp. ESI Hospital, Gumar Gala, Kolkata, Maheshtala", "West Bengal 700140, India"],
+    "pincode": "700140",
+    "zipCode":"700140", /* first few digits */
+    "numBeds" : "39",
+    "rating" : "3.4",
+},
+{
+    "name": "DM Hospitals Pvt. Ltd.",
+    "phoneNumber":"09836729186",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.46631199774888,
+        "longitude": 88.31050249654142,
+    },
+    "address":"113, James Long Sarani, Panchanan Tola, Thakurpukur, Kolkata, West Bengal 700063, India",
+    "addressLines":["113, James Long Sarani", "Panchanan Tola, Thakurpukur, Kolkata", "West Bengal 700063, India"],
+    "pincode": "700063",
+    "zipCode":"700063", /* first few digits */
+    "numBeds" : "Not Available",
+    "rating" : "3.3",
+},
+{
+    "name": "Mollargate hospital",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.51553919937048,
+        "longitude": 88.27010193191106,
+    },
+    "address":"G78C+53J, Mollargate Hospital Rd, Rampur, Santoshpur, Maheshtala, West Bengal 700142, India",
+    "addressLines":["G78C+53J, Mollargate Hospital Rd", "Rampur, Santoshpur, Maheshtala", "West Bengal 700142, India"],
+    "pincode": "700142",
+    "zipCode":"700142", /* first few digits */
+    "numBeds" : "Not Available",
+    "rating" : "3.5",
+},
+{
+    "name": "KASTURI DAS MEMORIAL SUPER SPECIALITY HOSPITAL",
+    "phoneNumber":"03371200000",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.51543815683898,
+        "longitude": 88.27064129800152,
+    },
+    "address":"19, Mollargate Hospital Rd, Santoshpur Govt Colony, Rampur, Santoshpur, Maheshtala, West Bengal 700142, India",
+    "addressLines":["19, Mollargate Hospital Rd", "Santoshpur Govt Colony, Rampur, Santoshpur, Maheshtala", "West Bengal 700142, India"],
+    "pincode": "700142",
+    "zipCode":"700142", /* first few digits */
+    "numBeds" : "16",
+    "rating" : "3.5",
+},
+{
+    "name": "Unipon Hospital & Medical Sciences Pvt Ltd",
+    "phoneNumber":"03330216666",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.540226607434292,
+        "longitude": 88.28922741393859,
+    },
+    "address":"M, 64, Paharpur Rd, Fatehpur Bazar, Fatepur Village, Garden Reach, Maheshtala, West Bengal 700024, India",
+    "addressLines":["M, 64, Paharpur Rd", "Fatehpur Bazar, Fatepur Village, Garden Reach, Maheshtala", "West Bengal 700024, India"],
+    "pincode": "700024",
+    "zipCode":"700024", /* first few digits */
+    "numBeds" : "19",
+    "rating" : "3.4",
+},
+{
+    "name": "Narayan Memorial Hospital",
+    "phoneNumber":"03366400000",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.49607044806312,
+        "longitude": 88.31766248786356,
+    },
+    "address":"601, Diamond Harbour Rd, Manton, Behala, Kolkata, West Bengal 700034, India",
+    "addressLines":["601, Diamond Harbour Rd", "Manton, Behala, Kolkata", "West Bengal 700034, India"],
+    "pincode": "700034",
+    "zipCode":"700034", /* first few digits */
+    "numBeds" : "20",
+    "rating" : "3.7",
+},
+{
+    "name": "U M R I HOSPITAL",
+    "phoneNumber":"09073699003",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.481188736588454,
+        "longitude": 88.31319134906788,
+    },
+    "address":"35/5, Bose Para Rd, Silpara, Purba Barisha, Kolkata, West Bengal 700008, India",
+    "addressLines":["35/5, Bose Para Rd", "Silpara, Purba Barisha, Kolkata", "West Bengal 700008, India"],
+    "pincode": "700008",
+    "zipCode":"700008", /* first few digits */
+    "numBeds" : "29",
+    "rating" : "4.0",
+},
+{
+    "name": "Peerless Hospital",
+    "phoneNumber":"03340111222",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.482381008474743,
+        "longitude": 88.39364874617729,
+    },
+    "address":"360, Panchasayar Rd, Sahid Smirity Colony, Pancha Sayar, Kolkata, West Bengal 700094, India",
+    "addressLines":["360, Panchasayar Rd", "Sahid Smirity Colony, Pancha Sayar, Kolkata", "West Bengal 700094, India"],
+    "pincode": "700094",
+    "zipCode":"700094", /* first few digits */
+    "numBeds" : "18",
+    "rating" : "4.1",
+},
+{
+    "name": "BMRI Hospital (P) Ltd",
+    "phoneNumber":"03333207114",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 22.455173271431196,
+        "longitude": 88.30254834366772,
+    },
+    "address":"F843+RVF, Diamond Harbour Rd, Near Thakurpukur 3A Bus Stand, Kumar Para, Thakurpukur, Purba Barisha, West Bengal 700063, India",
+    "addressLines":["F843+RVF, Diamond Harbour Rd", "Near Thakurpukur 3A Bus Stand, Kumar Para, Thakurpukur, Purba Barisha", "West Bengal 700063, India"],
+    "pincode": "700063",
+    "zipCode":"700063", /* first few digits */
+    "numBeds" : "24",
+    "rating" : "3.0",
+},
+
+
+
+]
+
+
